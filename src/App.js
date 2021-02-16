07d41fdeed96dfc891072aea2d00790d299705b6
treguv <vladislavtregubov00@gmail.com>
2021-02-15 20:47:25 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import About from "./components/About";
+
+function App() {
+  return (
+    //this is jsx ,not html
+    <div>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
